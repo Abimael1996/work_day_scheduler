775552ddb22442f244fb3c3764f63dec56ffb07d
abi <abimael.monarrez58@gmail.com>
2021-12-02 17:26:04 -0700

    fix issue with first timeblock

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ var hour = $(".hour");
 
 var textArea = $(".textarea");
 
+if (hour.eq(0).text() === today.format("hA")) {
+    textArea.eq(0).addClass("present");
+} else if (hour.eq(0).text() < today.format("hA")) {
+    textArea.eq(0).addClass("past");
+}  else {
+    textArea.eq(0).addClass("past");
+}
 
-for (var i = 0; i < hour.length; i++) {
+
+for (var i = 1; i < hour.length; i++) {
 if (hour.eq(i).text() === today.format("hA")) {
     textArea.eq(i).addClass("present");
 } else if (hour.eq(i).text() < today.format("hA")) {
     textArea.eq(i).addClass("past");
-} else if (hour.eq(0).text() > today.format("hA")) {
-    textArea.eq(i).addClass("past");
- } else {
+}  else {
     textArea.eq(i).addClass("future");
 }
 }
