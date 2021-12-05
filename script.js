3df4e3c4e166dfb264804f275064adc7e5501fbc
abi <abimael.monarrez58@gmail.com>
2021-12-04 19:58:34 -0700

    first draft done

--- script.js
+++ script.js
@@ -25,3 +25,84 @@ if (hour.eq(i).text() === today.format("hA")) {
 }
 }
 
+var storedInitialEvents = JSON.parse(localStorage.getItem("initialEvents"));
+
+console.log(storedInitialEvents);
+
+if (storedInitialEvents !=null) {
+
+for (var i = 0; i < textArea.length; i ++) {
+        
+    textArea.eq(i).text(storedInitialEvents[i]);
+    
+    }
+
+}
+
+
+
+var eventsArray = [];
+var indexArray = [];
+
+console.log(eventsArray);
+console.log(indexArray);
+
+var currentEvent = JSON.parse(localStorage.getItem("currentEvent"));
+var currentIndex = JSON.parse(localStorage.getItem("indexArray"));
+
+console.log(currentEvent);
+console.log(currentIndex);
+
+
+
+if (currentIndex !=null) {
+
+    for (var i = 0; i < currentIndex.length; i++) {
+    
+    textArea.eq(currentIndex[i] - 1).text(currentEvent[i])
+
+    }
+    }
+
+    
+
+    var initialEvents = [];
+
+    console.log(initialEvents);
+    
+    for (var i = 0; i < textArea.length; i++) {
+        
+        initialEvents.push(textArea.eq(i).text());
+    
+    }
+    
+    console.log(initialEvents);
+
+
+    localStorage.setItem("initialEvents", JSON.stringify(initialEvents));
+
+
+
+
+
+var button = $(".saveBtn")
+
+button.on("click", saveEvent);
+
+function saveEvent(event) {
+
+    var currentBtn = event.target;
+
+    var parent = currentBtn.closest("div");
+
+    var currentText = parent.children[1];
+
+    eventsArray.push(currentText.value);
+    indexArray.push(currentText.getAttribute("data-index"));
+
+
+    localStorage.setItem("currentEvent", JSON.stringify(eventsArray));
+    localStorage.setItem("indexArray", JSON.stringify(indexArray));
+
+}
+
